f8f17ea0d0e2b321b09b697d1980ffb8614e54a0
David Whiteside <starboarder2001@gmail.com>
2015-07-28 23:50:21 -0600

    setup default output directories

--- lib/ansibledocgen/cli.py
+++ lib/ansibledocgen/cli.py
@@ -5,9 +5,9 @@ class Cli(object):
 	def __init__(self):
 	    parser = optparse.OptionParser()
 	    parser.add_option("-p", "--project", dest="project",
-	    	help="Path to Ansible project", metavar="PROJECT")
+	    	help="Path to Ansible project", metavar="PROJECT", default="./")
 	    parser.add_option("-d", "--dest", dest="dest",
-	    	help="Path to write documentation", metavar="DEST")
+	    	help="Path to write documentation", metavar="DEST", default="./")
 	    parser.add_option("-s", "--style", dest="style",
 	    	help="Choose the format for the documentation", metavar="STYLE", default="markup")
 	    (self.options, self.args) = parser.parse_args()
