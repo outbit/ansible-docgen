1ab3953bc7e12ce6c97bfe475478dc0130088f99
David Whiteside <starboarder2001@gmail.com>
2015-07-31 22:03:01 -0600

    renamed from a variable name like file, too generic

--- lib/ansibledocgen/formatter/markup.py
+++ lib/ansibledocgen/formatter/markup.py
@@ -1,17 +1,18 @@
 class FormatterMarkup(object):
     def __init__(self, parserdata):
         self.parserdata = parserdata
-        """ Example:
+        """ Example parserdata:
         [ { "author": "test",
         "description": "test2",
         "task_names": ["str1", "str2"],
         "rolename" : None,
         "relative_path" : "./myplaybook.yml"}, ] """
+
         self.parse_data()
 
     def parse_data(self):
         for sourcefile in self.parserdata:
             self.write_doc(sourcefile)
 
-    def write_doc(self, file):
-        print(file)
+    def write_doc(self, sourcefile):
+        print(sourcefile)
