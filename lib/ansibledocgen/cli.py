940120b725b95b31cb1b51d3c45ec0156887b898
David Whiteside <starboarder2001@gmail.com>
2015-08-01 14:59:48 -0600

    fixed #7. removed unused cli options.

--- lib/ansibledocgen/cli.py
+++ lib/ansibledocgen/cli.py
@@ -1,4 +1,5 @@
 import optparse
+import sys
 from ansibledocgen.parser.dir import DirParser
 from ansibledocgen.formatter.markup import FormatterMarkup
 
@@ -7,12 +8,20 @@ class Cli(object):
         parser = optparse.OptionParser()
         parser.add_option("-p", "--project", dest="project",
             help="Path to Ansible project", metavar="PROJECT", default="./")
-        parser.add_option("-d", "--dest", dest="dest",
-            help="Path to write documentation", metavar="DEST", default="./")
         parser.add_option("-s", "--style", dest="style",
-            help="Choose the format for the documentation", metavar="STYLE", default="markup")
+            help="Choose the format for the documentation. Default is markup. Example: --style=[markup]", metavar="STYLE", default="markup")
         (self.options, self.args) = parser.parse_args()
 
     def run(self):
+        # Parse Roles and Playbooks
         self.dirparser = DirParser(self.options.project)
-        self.formatter = FormatterMarkup(self.dirparser.get_parserdata(), self.options.project)
+
+        # Based on chosen style, use the associated formatter
+        if self.options.style == "markup":
+            self.formatter = FormatterMarkup(self.dirparser.get_parserdata(), self.options.project)
+        else:
+            print("Error: Use of an unsupported style. The supported styles are: markup")
+            sys.exit(1)
+
+        sys.exit(0)
+
