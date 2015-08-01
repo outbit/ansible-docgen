bf91100ebb3448847396851842b1c39c88d7bb19
David Whiteside <starboarder2001@gmail.com>
2015-07-31 21:41:44 -0600

    updated documentation

--- lib/ansibledocgen/parser/playbook.py
+++ lib/ansibledocgen/parser/playbook.py
@@ -13,22 +13,27 @@ class PlaybookParser(object):
 
     def parse_playbook(self, playbook):
         with open(playbook) as f:
+            # Get Rolename from filepath
             rolename = None
             if self.is_role:
                 m = re.match(r".*roles/(.*?)/.*", playbook)
                 if m:
                     rolename = m.group(1)
 
+            # Setup Playbook Metadata
             playbookentry = {}
             playbookentry["task_names"] = []
             playbookentry["relative_path"] = playbook
+            playbookentry["rolename"] = rolename
+
+            # Read file content into data
+            data = f.read()
 
             # TEST
             playbookentry["author"] = "Test Author"
             playbookentry["description"] = "Test Description"
-            playbookentry["rolename"] = rolename
 
-            data = f.read()
+            # Parse Task Names from playbook
             for task in yaml.load(data):
                 for key in task:
                     if key == "name":
