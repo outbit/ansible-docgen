dbb31e77308e86db2198bcedd9aee87b86b65a03
David Whiteside <starboarder2001@gmail.com>
2015-08-02 16:15:34 -0600

    added unit test coverage for playbook parsing. fixed #3

---
+++ test/units/test_playbook.py
@@ -0,0 +1,31 @@
+import nose
+import unittest
+import sys
+import os
+from ansibledocgen.parser.playbook import PlaybookParser
+
+class TestPlaybook(unittest.TestCase):
+    def test_parser_playbook(self):
+        localdir = os.path.dirname(os.path.realpath(__file__))
+        testfile = os.path.join(localdir, ".output/testplaybook.yml")
+        with open(testfile, "w") as f:
+             f.write("---\n")
+             f.write("# Author: Me\n")
+             f.write("# description: this is a test\n")
+             f.write("- name: 'Install Apache'\n")
+             f.write("  yum: name=httpd state=installed\n")
+
+        playbook = PlaybookParser([testfile], is_role=False)
+
+        playbook.parse_playbooks()
+
+        # One Element in Array
+        assert( len(playbook.parserdata) == 1 )
+
+        assert( "author" in playbook.parserdata[0] )
+
+        assert( "description" in playbook.parserdata[0] )
+
+        assert( "task_names" in playbook.parserdata[0] )
+
+        assert( playbook.parserdata[0]["rolename"] is None)
