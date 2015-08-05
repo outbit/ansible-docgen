bdd82b97c8770a760529d8f14ef0121bb90dc47e
David Whiteside <starboarder2001@gmail.com>
2015-08-04 22:20:08 -0600

    test when playbook is part of a role

--- test/units/test_playbook.py
+++ test/units/test_playbook.py
@@ -30,3 +30,34 @@ class TestPlaybook(unittest.TestCase):
         assert( "task_names" in playbook.parserdata[0] )
 
         assert( playbook.parserdata[0]["rolename"] is None)
+
+    def test_parser_role_playbook(self):
+        # Find Directory Of This Test
+        localdir = os.path.dirname(os.path.realpath(__file__))
+        # Create Tasks Directory
+        if not os.path.exists(os.path.join(localdir, ".output/tasks/")):
+            os.makedirs(os.path.join(localdir, ".output/tasks/"))
+        # Set Output File
+        testfile = os.path.join(localdir, ".output/tasks/main.yml")
+        with open(testfile, "w") as f:
+             f.write("---\n")
+             f.write("# Author: Me\n")
+             f.write("# description: this is a test\n")
+             f.write("- name: 'Install Apache'\n")
+             f.write("  yum: name=httpd state=installed\n")
+
+        playbook = PlaybookParser([testfile], is_role=True)
+
+        playbook.parse_playbooks()
+
+        # One Element in Array
+        assert( len(playbook.parserdata) == 1 )
+
+        assert( "author" in playbook.parserdata[0] )
+
+        assert( "description" in playbook.parserdata[0] )
+
+        assert( "task_names" in playbook.parserdata[0] )
+
+        assert( playbook.parserdata[0]["rolename"] == ".output" )
+
