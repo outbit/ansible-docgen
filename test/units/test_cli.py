012fba3b96f483dc1d58f45f7c2c47e72648feb6
David Whiteside <starboarder2001@gmail.com>
2015-08-04 22:07:43 -0600

    added a few unit tests

--- test/units/test_cli.py
+++ test/units/test_cli.py
@@ -4,14 +4,24 @@ import unittest
 import sys
 
 class TestCli(unittest.TestCase):
-    def test_help(self):
-        sys.argv[1:] = "-h"
-        clivar = Cli()
-        assert("h" in clivar.args)
-
     def test_project(self):
+        # Test Specified Path
         sys.argv[1] = "-p"
         sys.argv[2] = "testdir"
-        clivar = Cli()
-        assert(clivar.project is "testdir")
+        cli = Cli()
+        assert(cli.project == "testdir/")
+        assert(cli.style == "markup")
+
+        # Test No Path Given
+        sys.argv[1] = ""
+        sys.argv[2] = ""
+        cli = Cli()
+        assert(cli.project == "./")
+        assert(cli.style == "markup")
+
+    def test_style(self):
+        sys.argv[1] = "-s"
+        sys.argv[2] = "markup"
+        cli = Cli()
+        assert(cli.style == "markup")
 
