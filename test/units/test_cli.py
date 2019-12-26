24078ca4c4f1cc481b1eef1b60d78444880c90bb
David Whiteside <david.whiteside@nrel.gov>
2019-12-26 08:36:22 -0700

    fixed unit tests

--- test/units/test_cli.py
+++ test/units/test_cli.py
@@ -17,15 +17,15 @@ class TestCli(unittest.TestCase):
         assert(cli.style == "markup")
 
         # Test No Path Given
-        sys.argv[1] = ""
-        sys.argv[2] = ""
+        sys.argv = [sys.argv[0]]
         cli = Cli()
         assert(cli.project == "./")
         assert(cli.style == "markup")
 
     def test_style(self):
-        sys.argv[1] = "-s"
-        sys.argv[2] = "markup"
+        sys.argv = [sys.argv[0]]
+        sys.argv.append("-s")
+        sys.argv.append("markup")
         cli = Cli()
         assert(cli.style == "markup")
 
