4209b93244adb028fcc994edfada6a15041f6dda
David Whiteside <david@davidwhiteside.com>
2023-09-24 13:45:09 -0600

    update python version support

--- setup.py
+++ setup.py
@@ -40,6 +40,9 @@ setup(
         'Natural Language :: English',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
@@ -48,7 +51,7 @@ setup(
         'Topic :: System :: Systems Administration',
         'Topic :: Utilities',
     ],
-    python_requires='>=3.10',
+    python_requires='>=3.7',
     scripts=[
         'bin/ansible-docgen',
     ],
